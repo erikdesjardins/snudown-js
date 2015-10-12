ae126471c3d2682c97dbffa956638f2bc2532403
Erik <erikdesjardins@users.noreply.github.com>
2015-10-12 13:17:12 -0400

    use a header file for `html_entities`

---
+++ src/html_entities.h
@@ -0,0 +1,16 @@
+#ifndef HTML_ENTITIES_H
+#define HTML_ENTITIES_H
+
+#include <stdlib.h>
+#include <stdint.h>
+
+const uint32_t MAX_NUM_ENTITY_VAL;
+
+const size_t MAX_NUM_ENTITY_LEN;
+
+int is_valid_numeric_entity(uint32_t entity_val);
+
+const char* is_allowed_named_entity (register const char *str, register unsigned int len);
+
+#endif
+
