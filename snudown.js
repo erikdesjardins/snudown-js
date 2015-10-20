b2fb3ca4208d4f089df11c26c0a3464b2ed9c802
Erik <erikdesjardins@users.noreply.github.com>
2015-10-20 19:52:23 -0400

    improve snudown.js documentation

--- snudown.js
+++ snudown.js
@@ -26,7 +26,7 @@ const RENDERER_WIKI = Module.ccall('wiki_renderer', 'number');
 const __markdown = Module.cwrap('snudown_md', 'string', ['number', 'number', 'number', 'string', 'string', 'number', 'number']);
 
 function _markdown(text, nofollow, target, toc_id_prefix, renderer, enable_toc) {
-	// not using Emscripten's automatic string handling since 'text'.length is unreliable for UTF-8 (e.g. e accent)
+	// not using Emscripten's automatic string handling since 'text'.length is unreliable for UTF-8
 	const size = Module.lengthBytesUTF8(text); // excludes null terminator
 	const buf = Module.allocate(Module.intArrayFromString(text), 'i8', Module.ALLOC_STACK);
 	return __markdown(buf, size, nofollow, target, toc_id_prefix, renderer, enable_toc);
@@ -34,8 +34,8 @@ function _markdown(text, nofollow, target, toc_id_prefix, renderer, enable_toc) 
 
 /**
  * Render markdown <tt>text</tt> to an HTML string.
- * Arguments may be passed positionally: <tt>markdown('hi', true, '_top', '', RENDERER_WIKI, true)</tt>
- * or by name in a single object: <tt>markdown({ text: 'hi', renderer: RENDERER_WIKI })</tt>
+ * Arguments may be passed positionally: <tt>markdown('hi', true, '_top', '', RENDERER_WIKI, true)</tt>,
+ * or by name in a single object: <tt>markdown({ text: 'hi', renderer: RENDERER_WIKI })</tt>.
  * Equivalent to python: <tt>markdown</tt>.
  * @param {string} text
  * @param {boolean} [nofollow=false] Whether to add <tt>rel="nofollow"</tt> to all links.
@@ -50,7 +50,7 @@ function markdown({ text = arguments[0], nofollow = arguments[1], target = argum
 }
 
 /**
- * Equivalent to {@link markdown} with the <tt>renderer</tt> param omitted, and always set to {@link RENDERER_WIKI}.
+ * Equivalent to {@link markdown}, but always uses {@link RENDERER_WIKI}.
  * @param {string} text
  * @param {boolean} [nofollow=false]
  * @param {string} [target=null]
