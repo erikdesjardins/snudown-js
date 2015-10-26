af83987b0cd2171c5cb198625ba3dd7b2945ab6d
Erik <erikdesjardins@users.noreply.github.com>
2015-10-26 16:25:33 -0400

    use camelcase in exposed API this isn't Python anymore

--- snudown.js
+++ snudown.js
@@ -40,13 +40,13 @@ function _markdown(text, nofollow, target, toc_id_prefix, renderer, enable_toc) 
  * @param {string} text
  * @param {boolean} [nofollow=false] Whether to add <tt>rel="nofollow"</tt> to all links.
  * @param {string} [target=null] The <tt>target</tt> property of all links.
- * @param {string} [toc_id_prefix=null] Added to the <tt>id</tt> of each TOC link, i.e. <tt>#PREFIXtoc_0</tt>.
+ * @param {string} [tocIdPrefix=null] Added to the <tt>id</tt> of each TOC link, i.e. <tt>#PREFIXtoc_0</tt>.
  * @param {number} [renderer=RENDERER_USERTEXT]
- * @param {boolean} [enable_toc=false] Whether to create a table of contents (Reddit generates the TOC separately).
+ * @param {boolean} [enableToc=false] Whether to create a table of contents (Reddit generates the TOC separately).
  * @returns {string} The rendered HTML.
  */
-function markdown({ text = arguments[0], nofollow = arguments[1], target = arguments[2], toc_id_prefix = arguments[3], renderer = arguments[4] === undefined ? RENDERER_USERTEXT : arguments[4], enable_toc = arguments[5] } = {}) {
-	return _markdown(text, nofollow, target, toc_id_prefix, renderer, enable_toc);
+function markdown({ text = arguments[0], nofollow = arguments[1], target = arguments[2], tocIdPrefix = arguments[3], renderer = arguments[4] === undefined ? RENDERER_USERTEXT : arguments[4], enableToc = arguments[5] } = {}) {
+	return _markdown(text, nofollow, target, tocIdPrefix, renderer, enableToc);
 }
 
 /**
@@ -54,12 +54,12 @@ function markdown({ text = arguments[0], nofollow = arguments[1], target = argum
  * @param {string} text
  * @param {boolean} [nofollow=false]
  * @param {string} [target=null]
- * @param {string} [toc_id_prefix=null]
- * @param {boolean} [enable_toc=false]
+ * @param {string} [tocIdPrefix=null]
+ * @param {boolean} [enableToc=false]
  * @returns {string} The rendered HTML.
  */
-function markdownWiki({ text = arguments[0], nofollow = arguments[1], target = arguments[2], toc_id_prefix = arguments[3], enable_toc = arguments[4] } = {}) {
-	return _markdown(text, nofollow, target, toc_id_prefix, RENDERER_WIKI, enable_toc);
+function markdownWiki({ text = arguments[0], nofollow = arguments[1], target = arguments[2], tocIdPrefix = arguments[3], enableToc = arguments[4] } = {}) {
+	return _markdown(text, nofollow, target, tocIdPrefix, RENDERER_WIKI, enableToc);
 }
 
 exports.version = version;
