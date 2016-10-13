8a4c00fb230d125a4192713680be3ef7b9415ae2
Erik <erikdesjardins@users.noreply.github.com>
2016-10-12 22:25:08 -0400

    limit exported runtime methods

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ gperf src/html_entities.gperf > build/html_entities.c
 files="snudown.c src/autolink.c src/buffer.c src/markdown.c src/stack.c html/houdini_href_e.c html/houdini_html_e.c html/html.c build/html_entities.c"
 include=("src" "html")
 exported=("default_renderer" "wiki_renderer" "snudown_md" "version" "main")
+exported_runtime=("ccall" "cwrap" "lengthBytesUTF8" "allocate" "intArrayFromString" "ALLOC_NORMAL" "Pointer_stringify" "UTF8ToString")
 options=("NO_EXIT_RUNTIME=1" "NO_FILESYSTEM=1" "NO_DYNAMIC_EXECUTION=1")
 
 if [ "$1" = "-d" ] || [ "$1" = "--debug" ]; then
@@ -31,11 +32,16 @@ for i in "${include[@]}"; do
 done
 
 cmd="$cmd -s EXPORTED_FUNCTIONS=["
-
 for i in "${exported[@]}"; do
 	cmd="$cmd'_$i',"
 done
+cmd="${cmd:0:${#cmd} - 1}" # trim last comma
+cmd="$cmd]"
 
+cmd="$cmd -s EXPORTED_RUNTIME_METHODS=["
+for i in "${exported_runtime[@]}"; do
+	cmd="$cmd'$i',"
+done
 cmd="${cmd:0:${#cmd} - 1}" # trim last comma
 cmd="$cmd]"
 
