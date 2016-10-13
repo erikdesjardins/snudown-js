4cfc9d7b6a967620c2840fdb234cde16fc50f0c3
Erik <erikdesjardins@users.noreply.github.com>
2016-10-12 23:09:37 -0400

    use -Oz and LLVM link-time optimization (--llvm-lto)
    Even though this technically should be less optimized than -O3, it appears to be faster at runtime (and is definitely smaller), perhaps because V8 better handles smaller functions with less inlining.

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ if [ "$1" = "-d" ] || [ "$1" = "--debug" ]; then
 	optimization=""
 else
 	echo "*** DEBUGGING OFF : use -d to enable ***"
-	optimization="-O3 --memory-init-file 0"
+	optimization="-Oz --llvm-lto 1 --memory-init-file 0"
 fi
 
 cmd="emcc $files -o build/snudown.js --pre-js header.js --post-js footer.js $optimization"
