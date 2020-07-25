83f9761d0e78474dc03ad8ff65d54bc5406452b5
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2020-07-25 12:05:27 -0400

    fix is_allowed_named_entity type

--- src/html_entities.h
+++ src/html_entities.h
@@ -10,7 +10,7 @@ const size_t MAX_NUM_ENTITY_LEN;
 
 int is_valid_numeric_entity(uint32_t entity_val);
 
-const char* is_allowed_named_entity (register const char *str, register unsigned int len);
+const char* is_allowed_named_entity (register const char *str, register size_t len);
 
 #endif
 
